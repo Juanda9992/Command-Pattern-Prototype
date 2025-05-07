dcc1f91e53485d6bb04dcdd0264f008cf4740064
Juanda999 <juanda999games@gmail.com>
2025-05-07 00:17:42 -0500

    Undo button deletes last action

--- Assets/Scripts/UI/Action_Buttons_UI_Manager.cs
+++ Assets/Scripts/UI/Action_Buttons_UI_Manager.cs
@@ -7,10 +7,20 @@ public class Action_Buttons_UI_Manager : MonoBehaviour
     [SerializeField] private Transform buttonsParent;
     [SerializeField] private GameObject buttonsPrefab;
 
+    [SerializeField] private List<GameObject> allButtonsStored;
+
     public void InstantiateButton(ActionType actionType)
     {
         GameObject currentButton = Instantiate(buttonsPrefab,buttonsParent);
         currentButton.SetActive(true);
         currentButton.GetComponent<Action_Container_Button>().SetActionType(actionType);
+
+        allButtonsStored.Add(currentButton);
+    }
+
+    public void RemoveLastAction()
+    {
+        Destroy(allButtonsStored[allButtonsStored.Count-1]);
+        allButtonsStored.RemoveAt(allButtonsStored.Count-1);
     }
 }
