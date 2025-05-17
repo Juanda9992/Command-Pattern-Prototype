e5b3b88519170131f5a7a6738ff47282efe07a1a
Juanda999 <juanda999games@gmail.com>
2025-05-17 00:17:10 -0500

    Turn on movement buttons based on game rules

--- Assets/Scripts/Managers/Gameflow/Level_Rules_Manager.cs
+++ Assets/Scripts/Managers/Gameflow/Level_Rules_Manager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Level_Rules_Manager : MonoBehaviour
 {
@@ -9,9 +10,17 @@ public class Level_Rules_Manager : MonoBehaviour
 
     [SerializeField] private Transform playerTransform;
 
+    [SerializeField] private GameObject leftButton, rightButton, moveButton, interactButton;
+
     public void LoadRules()
     {
         playerTransform.position = level_Rules.playerPosition;
         playerTransform.rotation = Quaternion.Euler(0, level_Rules.playerRotation, 0);
+
+
+        leftButton.SetActive(level_Rules.rotateLeftButton);
+        rightButton.SetActive(level_Rules.rotateTightButton);
+        moveButton.SetActive(level_Rules.moveNextButton);
+        interactButton.SetActive(level_Rules.interactButton);
     }
 }
