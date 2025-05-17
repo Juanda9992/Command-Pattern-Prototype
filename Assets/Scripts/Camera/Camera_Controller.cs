8101909ed205064faee67876b2b126ebe8f85530
Juanda999 <juanda999games@gmail.com>
2025-05-16 23:44:23 -0500

    Add camera panning

--- Assets/Scripts/Camera/Camera_Controller.cs
+++ Assets/Scripts/Camera/Camera_Controller.cs
@@ -7,6 +7,7 @@ public class Camera_Controller : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Vector3 followOffset;
+    [SerializeField] private Vector3 defaultOffset;
 
     [Header("Zoom Settings")]
     [SerializeField] private float minZoom;
@@ -17,14 +18,45 @@ public class Camera_Controller : MonoBehaviour
     void Awake()
     {
         mainCamera = GetComponent<Camera>();
+        followOffset = defaultOffset;
     }
     void LateUpdate()
     {
-        transform.position = player.position + followOffset;
+        transform.DOMove(player.position + followOffset,0.5f);
 
         currentZoom += Input_Actions_Manager.instance.GetCameraScroll();
         currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
 
+        PanCamera();
+
         mainCamera.DOOrthoSize(currentZoom, 0.4f);
     }
+
+    private void PanCamera()
+    {
+        Vector2 movingVectorreference = defaultOffset;
+        switch (Input_Actions_Manager.instance.GetMouseBoundsPos())
+        {
+            case Input_Actions_Manager.MouseEdgePosition.Bottom:
+                followOffset.z = defaultOffset.z - 1;
+                followOffset.x = defaultOffset.x - 1;
+                break;
+            case Input_Actions_Manager.MouseEdgePosition.Upper:
+                followOffset.z = defaultOffset.z + 1;
+                followOffset.x = defaultOffset.x + 1;
+                break;
+            case Input_Actions_Manager.MouseEdgePosition.Left:
+                followOffset.x = defaultOffset.x - 1;
+                followOffset.z = defaultOffset.z + 1;
+                break;
+            case Input_Actions_Manager.MouseEdgePosition.Right:
+                followOffset.x = defaultOffset.x + 1;
+                followOffset.z = defaultOffset.z - 1;
+                break;
+            case Input_Actions_Manager.MouseEdgePosition.None:
+                followOffset = defaultOffset;
+                break;
+        }
+    }
 }
+
--- Assets/Scripts/Managers/Input/Input_Actions_Manager.cs
+++ Assets/Scripts/Managers/Input/Input_Actions_Manager.cs
@@ -7,13 +7,53 @@ public class Input_Actions_Manager : MonoBehaviour
 {
     [SerializeField] private InputActionAsset inputActions;
     public static Input_Actions_Manager instance;
+
+    [SerializeField] private float screenBorderThreshold;
+
+    public enum MouseEdgePosition { Upper, Bottom, Left, Right, None }
     void Awake()
     {
         instance = this;
         inputActions.Enable();
+
+        Debug.Log(Screen.width + " " + Screen.height);
+    }
+
+    void Update()
+    {
     }
     public float GetCameraScroll()
     {
         return inputActions.FindAction("Scroll").ReadValue<float>();
+    }
+
+    public Vector2 GetMousePos()
+    {
+        return inputActions.FindAction("Mouse Position").ReadValue<Vector2>();
+    }
+
+    public MouseEdgePosition GetMouseBoundsPos()
+    {
+        if (GetMousePos().x > Screen.width * screenBorderThreshold)
+        {
+            return MouseEdgePosition.Right;
+        }
+
+        if (GetMousePos().x < Screen.width - (Screen.width * screenBorderThreshold))
+        {
+            return MouseEdgePosition.Left;
+        }
+
+        if (GetMousePos().y > Screen.height * screenBorderThreshold)
+        {
+            return MouseEdgePosition.Upper;
+        }
+
+        if (GetMousePos().y < Screen.height - (Screen.height * screenBorderThreshold))
+        {
+            return MouseEdgePosition.Bottom;
+        }
+
+        return MouseEdgePosition.None;
     }
 }
